d814f21b32ea66b5dc562d6a36e2507b000e74d2
Alan <alanzambie@gmail.com>
2020-10-13 16:35:36 -0500

    Practica de la clase 13 de Octubre de 2020

--- Practica4_Condicionales.R
+++ Practica4_Condicionales.R
@@ -1,9 +1,12 @@
-
-#Practica 4 Condicionales
+################################################################################
+##########  Practica 4 Clase - Condicionales ###################################
+################################################################################
 
 #Primera condicional
+#Determinar si 4 es mayor a 5
+#Ejecutamos ELSE
 
-
+#¿4 es mayor a 5?
 if (4 > 5) {
   print('Es verdad')
 } else {
@@ -12,24 +15,32 @@ if (4 > 5) {
 
 
 #Segunda condicional
+#Almacenamos dos variables en objetos a y b
+#No ejecutamos ELSE
 a <- 10
 b <- 12
 
+#¿a es menor o igual a b?
 if (a <= b){
   print('Si, es verdad')
 }
 
 
-#Preguntamos si 5 es menor a 7 y si 4 es menor a 3
+#Tercera condicional
+#Preguntamos si 5 es menor a 7 o si 4 es menor a 3
+#Debe cumplir por lo menos una condición
 
-if ( 5<7 | 4<3 ){
+if ( 5 < 7 | 4 < 3  ){
   print('Es verdad')
 }else {
   print('No es verdad')
 }
 
 
-#Ciclo For 
+#Utilizamos un Ciclo For con if anidado en su esructura
+#Cada vez que el iterador realice una vuelta:
+  #Comprueba que la variable actual 'iterador' sea mayor o igual a 4
+  #De lo contrario ELSE demuestra que no es mayor
 
 for (iterador in 1:5){
   #print(paste('Hola', iterador))

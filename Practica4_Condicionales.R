3b626f27c36000f4f10d67b63223383fcf66b3c8
AllanZamb <59672234+AllanZamb@users.noreply.github.com>
2020-10-13 15:04:20 -0500

    Add files via upload

---
+++ Practica4_Condicionales.R
@@ -0,0 +1,47 @@
+
+#Practica 4 Condicionales
+
+#Primera condicional
+
+
+if (4 > 5) {
+  print('Es verdad')
+} else {
+  print('es falso')
+}
+
+
+#Segunda condicional
+a <- 10
+b <- 12
+
+if (a <= b){
+  print('Si, es verdad')
+}
+
+
+#Preguntamos si 5 es menor a 7 y si 4 es menor a 3
+
+if ( 5<7 | 4<3 ){
+  print('Es verdad')
+}else {
+  print('No es verdad')
+}
+
+
+#Ciclo For 
+
+for (iterador in 1:5){
+  #print(paste('Hola', iterador))
+  
+  if ( iterador >= 4 ){
+    print('Si, es mayor o igual')
+  }
+  else {
+    print('No es mayor')
+  }
+  
+}
+
+
+

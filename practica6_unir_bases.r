0ae0740c5fc8d031ed49f70a54eaa3fbb130c186
Allan Zambrano <59672234+AllanZamb@users.noreply.github.com>
2020-11-10 14:23:59 -0600

    Update practica6_unir_bases.r

--- practica6_unir_bases.r
+++ practica6_unir_bases.r
@@ -1,14 +1,26 @@
 library (plyr)
-#Cargamos las bases
-base1 <- read.csv("/Users/salademonitoreo1/Documents/BASES/base1.csv")
-
-#Bases desde un sitio web
-base1 <-read_csv("https://raw.githubusercontent.com/AllanZamb/CURSO-R/main/BASES/base1.csv")
-base2 <-read_csv("https://raw.githubusercontent.com/AllanZamb/CURSO-R/main/BASES/base2.csv")
-base3 <-read_csv("https://raw.githubusercontent.com/AllanZamb/CURSO-R/main/BASES/base3.csv")
-base4 <-read_csv("https://raw.githubusercontent.com/AllanZamb/CURSO-R/main/BASES/base4.csv")
-base5 <-read_csv("https://raw.githubusercontent.com/AllanZamb/CURSO-R/main/BASES/base5.csv")
 
 
-#rbind.fill nos ayuda a combinar por filas
-BASE <- rbind.fill(base1,base2,base3,base4,base5)
+#Vectores Primarios
+nombre_paciente <- c('ALAN', 'GERARDO', 'LUIS', 'GUILLERMO', 'MARÍA')
+edad_paciente <- c(31,34,65,44,66)
+sexo_paciente <- c('M','M','M','M','F')
+
+#Lista
+datos_del_paciente <- list(nombre_paciente,edad_paciente, sexo_paciente)
+
+#DataFrame
+datos_pacientes <- as.data.frame(datos_del_paciente)
+colnames(datos_pacientes) <- c("Nombre", 'Edad', "Sexo") 
+
+base1 <- read.csv('https://raw.githubusercontent.com/AllanZamb/CURSO-R/main/BASES/base1.csv')
+base2 <- read.csv('https://raw.githubusercontent.com/AllanZamb/CURSO-R/main/BASES/base2.csv')
+base3 <- read.csv('https://raw.githubusercontent.com/AllanZamb/CURSO-R/main/BASES/base3.csv')
+base4 <- read.csv('https://raw.githubusercontent.com/AllanZamb/CURSO-R/main/BASES/base4.csv')
+base5 <- read.csv('https://raw.githubusercontent.com/AllanZamb/CURSO-R/main/BASES/base5.csv')
+
+#Metemos las bases en listas
+BASES_LISTAS <- list(base1, base2, base3, base4, base5)
+
+#UNIMOS las bases de datos en un objeto
+BASE_PACIENTES_COVID <- rbind.fill(BASES_LISTAS)

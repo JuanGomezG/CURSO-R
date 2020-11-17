7df8d7c92b7b1217b4817a6cd000c34b0ad21e20
Alan <alanzambie@gmail.com>
2020-11-17 13:01:29 -0600

    ok

--- Practica_8 DataFrame.R
+++ Practica_8 DataFrame.R
@@ -1,24 +1,28 @@
+#Cargamos las librerías que vamos a utilizar
 library(data.table)
 library(plyr)
 library(readr)
 
-
 paises <- read.csv("https://covid19.who.int/WHO-COVID-19-global-data.csv")
 
+#DESINTEGRAMOS EL DATASET
+#Dividimos la base de datos en función de las fechas reportadas
+dfs_paises <- split(paises, paises$Date_reported) 
 
-dfs <- split(paises, paises$ï..Date_reported) # list of dfs
-# use numbers as file names
+#Lapply se aplica a una función para cada elemento de la lista para enviar cada una de las listas a un archivo de escritura DataFrame
 
-lapply(names(dfs),
-       function(x){write.csv(dfs[[x]], paste0(x,".csv"),
+lapply(names(dfs_paises),
+       function(x){write.csv(dfs_paises[[x]], paste0("BasesDataFrame/",x,".csv"),
                              row.names = FALSE)}) 
 
 
-files = list.files(pattern="*.csv")
 
+#INTEGRAMOS EL DATASET
+#Encontramos patrones en los archivos
+patrones_csv = list.files("BasesDataFrame/",pattern="*.csv")
 
-#Método 1
-dat_csv = ldply(files, read_csv)
+#Método 1 (sencillo)
+Data_Frame_Completo = ldply(paste0("BasesDataFrame/",patrones_csv), read_csv)
 
 #Método 2
 paises_list <- list()
@@ -28,4 +32,4 @@ for (i in 1:length(files)){
 paises_data <- rbind.fill(paises_list)
 
 
-
+?lapply

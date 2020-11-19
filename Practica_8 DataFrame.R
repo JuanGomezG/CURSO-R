ca520f7f93a5eedfc25ff76ca8a39f62b94e9605
Alan <alanzambie@gmail.com>
2020-11-19 15:21:28 -0600

    ok

--- Practica_8 DataFrame.R
+++ Practica_8 DataFrame.R
@@ -7,15 +7,14 @@ paises <- read.csv("https://covid19.who.int/WHO-COVID-19-global-data.csv")
 
 #DESINTEGRAMOS EL DATASET
 #Dividimos la base de datos en función de las fechas reportadas
-dfs_paises <- split(paises, paises$Date_reported) 
+dfs_paises <- split(paises, paises$Country) 
+
 
 #Lapply se aplica a una función para cada elemento de la lista para enviar cada una de las listas a un archivo de escritura DataFrame
 
 lapply(names(dfs_paises),
        function(x){write.csv(dfs_paises[[x]], paste0("BasesDataFrame/",x,".csv"),
                              row.names = FALSE)}) 
-
-
 
 #INTEGRAMOS EL DATASET
 #Encontramos patrones en los archivos
@@ -24,12 +23,10 @@ patrones_csv = list.files("BasesDataFrame/",pattern="*.csv")
 #Método 1 (sencillo)
 Data_Frame_Completo = ldply(paste0("BasesDataFrame/",patrones_csv), read_csv)
 
-#Método 2
+#Método 2 (con iteracón)
 paises_list <- list()
 for (i in 1:length(files)){
   paises_list[[i]] <- read.csv(files[i])
 }
 paises_data <- rbind.fill(paises_list)
 
-
-?lapply
--- Practica_8 DataFrame2.R
+++ Practica_8 DataFrame2.R
@@ -1,20 +1,33 @@
+#Cargamos el DataFrame con url Data.world
 billboards <- read.csv("https://query.data.world/s/ujgyeerihykzmkhsvgq3kgs5bzrg6g")
+
 billboards <- read.csv("C:\\Users\\alan2\\Downloads\\billboard_2000_2018_spotify_lyrics.csv")
 
 billboards <- read.csv("C:\\Users\\alan2\\Downloads\\albumlist.csv")
 
+# Quitamos los NA
 billboards$year[is.na(billboards$year)] <- 2018L
 
-billboards_dfs <- split(billboards, billboards$Year) # list of dfs
+### DESINTEGRAMOS EL DATAFRAME
+# Cortamos el DataFrame por año
+billboards_dfs <- split(billboards, billboards$Year) 
 
+#Esta función la utilizamos para construir y escribir las bases de datos en archivos CSV 
 lapply(names(billboards_dfs),
-       function(x){write.csv(billboards_dfs[[x]], paste0(x,".csv"),
+       function(x){write.csv(billboards_dfs[[x]], paste0("BasesDataFrame/",x,".csv"),
                              row.names = FALSE)}) 
 
-files = list.files(pattern="*.csv")
+#INTEGRAMOS EL DATAFRAME A PARTIR DE LOS ARCHIVOS CSV
+patrones_csv = list.files("BasesDataFrame/",pattern="*.csv")
 
+#Método 1 (sencillo)
+Data_Frame_Completo = ldply(paste0("BasesDataFrame/",patrones_csv), read_csv)
+
+
+
+
+#Método 2 (con iteraciones)
 fechas_list<- list()
-
 for (i in 1:length(files)){
   fechas_list[[i]] <- read.csv(files[i])
 }
@@ -22,12 +35,3 @@ for (i in 1:length(files)){
 library(plyr)
 paises_data <- rbind.fill(fechas_list)
 
-
-library(tidyverse)
-library(factoextra)
-library(dplyr)
-library(ggplot)
-
-glimpse(billboards)
-
-ggcorr(billboards,label = T, label_size = 3)

5b3454b994fdb3047d031249a52783b5271ab93e
Allan Zambrano <59672234+AllanZamb@users.noreply.github.com>
2020-12-10 16:01:04 -0600

    Update Practica10_Dplyr_graficas.R

--- Practica10_Dplyr_graficas.R
+++ Practica10_Dplyr_graficas.R
@@ -1,7 +1,9 @@
+#Cargamos las librerías
 
 library(ggplot2)
 library(dplyr)
 library(gganimate)
+library (gifski)
 ################################################################################
 #Cargamos el Dataset de OMS
 paises<- read.csv("https://covid19.who.int/WHO-COVID-19-global-data.csv")
@@ -38,15 +40,24 @@ plot(paises$WHO_region,
      xlab = "Regiones",
      ylab = "Cantidad")
 
-
+boxplot(paises$New_cases~paises$WHO_region)
 ################################################################################
+
+#Ejercicio 2 .- Debes realizar la agrupación de tres variables (Fecha, País y Número de casos por día) y graficarlas (añadir animación)
+
 #Filtramos por región y acumulamos los casos nuevos
 paises_filtrado <- paises %>%  
   group_by(Fecha_Reporte = Date_reported, Region=WHO_region) %>% 
   summarise(Casos_Nuevos = sum(New_cases)) 
 
-paises_filtrado$Fecha_Reporte <- as.Date(paises_filtrado$Fecha_Reporte, format = "%Y-%m-%d")
 
+#Filtramos por pais y acumulamos los casos nuevos
+paises_filtrado2 <- paises %>%  
+  group_by(Fecha_Reporte = Date_reported, Pais = Country) %>% 
+  summarise(Casos_Nuevos = sum(New_cases)) 
+
+#Importante realizar la conversión a Date antes de mandar a ggplot
+paises_filtrado$Fecha_Reporte <- as.Date(paises_filtrado$Fecha_Reporte, format = "%Y-%m-%d")
 
 ################################################################################
 #Gráfica básica (boxplot)
@@ -54,67 +65,76 @@ ggplot(paises_filtrado,
        aes(x = Fecha_Reporte,
            y = Casos_Nuevos,
            fill = Region)) + 
-  #geom_line() +
   geom_boxplot()
 
-#Gráfica básica (lineas)
+
+#Gráfica básica (linea)
 ggplot(paises_filtrado,
        aes(x = Fecha_Reporte,
            y = Casos_Nuevos,
-           color = Region)) + 
-  geom_line() 
+           col = Region)) + 
+  geom_line() +
+  scale_color_manual(values = color)
  
-  
-################################################################################
-#Gráfico con elementos
-paises_filtrado <- paises %>%  
-  group_by(Fecha_Reporte = Date_reported, Region=WHO_region) %>% 
-  summarise(Casos_Nuevos = sum(New_cases)) 
 
-paises_filtrado$Fecha_Reporte <- as.Date(paises_filtrado$Fecha_Reporte, format = "%Y-%m-%d")
+################################################################################
+#Gráfica básica (lineas y puntos)
+ggplot(paises_filtrado,
+       aes(x = Fecha_Reporte,
+           y = Casos_Nuevos,
+           color = Region)) + 
+  geom_point() +
+  geom_line()
+
+################################################################################
+#Gráfico con elementos adicionales
 
 ggplot(paises_filtrado,
        aes(x = Fecha_Reporte,
            y = Casos_Nuevos,
            color = Region)) + 
-  geom_line(size =2) + 
-  geom_point(size =2 )+
-  #geom_boxplot()
-  
-  #labs(title= 'Casos de Covid en el mundo {frame_along}',
+  geom_line() + 
+  geom_point()+
+    #labs(title= 'Casos de Covid en el mundo {frame_along}',
   labs(title= 'Casos de Covid en el mundo',
        x = 'Fecha',
        y = 'Años de vida') +
-  
-  theme_minimal()
+  theme_minimal() + 
+  transition_reveal(Fecha_Reporte)
+
 #Animamos el gráfico con la función transition reveal
 #transition_reveal(Fecha_Reporte)
-################################################################################
-#Realizamos un merge con otra base de datos
-#Traemos solo los paises y la cantidad de nuevos casos
 
+################################################################################
+
+#Ejercicio 3 Debemos seleccionar los 10 paises con mas casos nuevos por cada millón de habitantes
+
+#Necesitamos la población para cruzar bases de datos
+
+#Seleccionamos solo los paises y la cantidad de nuevos casos
 casos_nuevos_pais <- paises %>% group_by(Country) %>%
   summarise(Casos_N = sum(New_cases)) %>%
   arrange(desc(Casos_N))
 
+#Realizamos un merge con otra base de datos
 poblacion <- read.csv("https://raw.githubusercontent.com/AllanZamb/CURSO-R/main/BasesDataFrame/pop_world.csv")
-
-poblacion <- poblacion %>% select(Country=country, pop, year)%>%
-  filter(year == 2007)
-
-
-nueva <- left_join(casos_nuevos_pais, poblacion, by= "Country")
-
-
+base_casos_combinada <- left_join(casos_nuevos_pais, poblacion, by= "Country")
 #Realizamos la mezcla de las tablas
-Casos_por_millon <- nueva %>% mutate(Casos_Nmillon = as.numeric(round(Casos_N / pop * 1000000,0)))  
+base_casos_combinada <- base_casos_combinada %>% mutate(Casos_Nmillon = as.numeric(round(Casos_N / pop * 1000000,0)))  
 
 
-#############################################################################3
-paises_filtrado_pais <- paises %>%  filter(Country == c('Mexico', "Denmark")) %>% 
+#Verificamos los diez primeros casos
+paises_mas_casos_millon <- base_casos_combinada$Country[1:10]
+################################################################################
+
+
+#Ejercicio 4 filtrar datos con los paises con mayor tasa de casos por mill´pn de habitantes
+paises_filtrado_pais <- paises %>%  filter(Country == paises_mas_casos_millon) %>% 
   group_by(Fecha_Reporte = Date_reported, Pais=Country) %>% 
-  summarise(Casos_Nuevos = sum(New_cases)) 
+  summarise(Casos_Nuevos = sum(New_cases)) %>% 
+  arrange(desc(Casos_Nuevos))
 
+#Convertimos a Date
 paises_filtrado_pais$Fecha_Reporte <- as.Date(paises_filtrado_pais$Fecha_Reporte, format = "%Y-%m-%d")
 
 ggplot(paises_filtrado_pais,
@@ -123,14 +143,19 @@ ggplot(paises_filtrado_pais,
            color = Pais)) +
   geom_bar(stat="identity") +
   geom_line() +
-  facet_grid(Pais~.) + 
-  transition_reveal(Fecha_Reporte)
+  transition_reveal(Fecha_Reporte) 
+  #facet_grid(Pais~.) + 
   
   #geom_line() +
   #geom_point()
 
 
+################################################################################
+
+
 ###############################################################################
+
+#Ejercicio 5
 #Creamos otro tipo de gráficas acumuladas por región
 library(ggplot2)
 
